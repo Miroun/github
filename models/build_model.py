13205321dd9edc1625d90738ebe786b44cc7376d
Roun <haroun@ammi.be>
2022-05-20 09:53:25 +0900

    test

---
+++ models/build_model.py
@@ -0,0 +1,16 @@
+from .featmodel import ImgModel, MultiHeadModel
+
+
+def build_model(config):
+    model_name = config.model
+    model_type = config.type
+    model_head = config.head
+    input_channel = config.input_channel
+    if model_name == "vgg":
+        model = ImgModel(input_channel=input_channel)
+    if model_name == "multi_head_vgg":
+        model = MultiHeadModel(input_channel=input_channel)
+    if model_name == "siamese":
+        model_name = SimpleSiameseModel(input_channel=input_channel)
+
+    return model
